506a1714af52482be4228270cc555bf14a01b99d
Fernando Silva <fernandorsilva9@gmail.com>
2017-09-22 23:39:20 +0100

    Add files via upload

--- Journal.py
+++ Journal.py
@@ -1,1 +1,88 @@
 # Journal
+
+# O que deseja fazer? Ver o plano, introduzir peso, introduzir treino ou obter dados estatisticos?
+
+# -----------------------
+
+
+# If "peso":
+    # input Introduza o seu peso
+
+# (A introduzir o peso na tabela)
+# (Calcular a média dos últimos 7 dias e do último mês)
+
+# print valor
+# print média da semana
+# print média do mês
+
+
+# ------------------------
+
+
+    # elif "plano":
+        # input Deseja criar um novo plano?
+        
+        # if "sim":
+            #(COPIAR CODIGO MATLAB E INTRODUZIR NA TABELA)
+            # else:
+            # Mostrar o que tenho que fazer nos próximos 7 dias
+    
+
+# ------------------------
+    
+    # elif "treino":
+        #input que dia é hoje?
+        #input tipo de treino?
+        
+            # if "velocidade":
+                #input series ou tempo?
+                    
+                #if "tempo":
+                    #input distância lenta
+                    #input distância rápida
+                    #input tempo decorrido
+                    
+                    
+                # (A calcular o velocidade)
+                # (comparar com a velocidade pre-determinada pelo plano e que deverá constar na tabela)
+                # print a dizer se SE < 5%
+                
+                #else:
+                    #input Series de que distância?
+                    #input Quantas series?
+                    #input de quantas series forem para meter os tempos de todas as series
+                
+                # (A calcular o somatório de todas as séries)
+                # (A calcular o velocidade)
+                # (comparar com a velocidade pre-determinada pelo plano e que deverá constar na tabela)
+                # print a dizer se SE < 5%
+            
+            # if "lento":
+               #input do tempo decorrido
+               #input da distância percorrida
+               
+               # (A calcular o velocidade)
+               # (comparar com a velocidade pre-determinada pelo plano e que deverá constar na tabela)
+               
+               # if dia é domingo:
+                   #(Dividir distância pela soma dos últimos 7 dias)
+               
+                # print do rácio distancia/soma dos últimos dias
+               # print a dizer se SE < 5%
+                
+    # elif "estatistica":
+        #input duração: semanal, quinzenal, 4 semanas, anual
+        
+        # (Computar médias de peso, distância percorrida, somatório de distância)
+        
+        # Print valores
+        # Criar gráficos para as variações de peso; distância percorrida; percentagem das long runs; percentagem de treino em velocidade
+        # Segundo o plano, quanto deveria ter corrido até agora? % de velocidade? 
+        
+        
+# Deseja ver mais alguma coisa?
+    # if "sim":
+        # Voltar lá acima (Ou seja, criar um while loop ou um for loop)
+        
+        #else:
+            #Acabar com o algoritmo, ou seja, break???
